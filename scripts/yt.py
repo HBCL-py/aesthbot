2e63efc96943c0ceba8922b94587b71a7589c5a2
AlexeiStukov <41208403+AlexeiStukov@users.noreply.github.com>
2018-07-15 02:05:43 -0400

    Update yt.py

--- scripts/yt.py
+++ scripts/yt.py
@@ -3,7 +3,7 @@ import urllib.parse
 import re
 
 def ytsearch(q):
-    query_string = urllib.parse.urlencode(q)
+    query_string = urllib.parse.urlencode({"search_query" : q})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
     return search_results
